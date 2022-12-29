d154f54f721cd28fc065c135afbb9886aa47ebbd
Hai Long <hailongkc1203@gmail.com>
2022-12-29 13:51:24 +0700

    READ - Hiển Thị Danh Sách Users

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,7 +1,66 @@
-const TableUsers = () => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import { fetchAllUser } from "../services/UserServices";
+
+const TableUsers = (props) => {
+
+    const [listUsers, setlistUsers] = useState([]);
+    useEffect(() => {
+        // code
+        getUsers();
+    }, [])
+
+    const getUsers = async () => {
+        let res = await fetchAllUser();
+        if (res && res.data && res.data.data) {
+            setlistUsers(res.data.data)
+        }
+    }
+
+    // avatar
+    // :
+    // "https://reqres.in/img/faces/7-image.jpg"
+    // email
+    // :
+    // "michael.lawson@reqres.in"
+    // first_name
+    // :
+    // "Michael"
+    // id
+    // :
+    // 7
+    // last_name
+    // :
+    // "Lawson"
+
+    console.log(">>> check listUserss", listUsers);
     return (
         <div>
-            Header
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
+                        return (
+                            <tr key={`users-${index}`}>
+                                <td>{item.id}</td>
+                                <td>{item.first_name}</td>
+                                <td>{item.last_name}</td>
+                                <td>{item.email}</td>
+                            </tr>
+                        )
+
+                    })}
+
+                </tbody>
+            </Table>
         </div>
     )
 }
---
+++ src/services/UserServices.js
@@ -0,0 +1,12 @@
+
+import axios from 'axios';
+
+
+const fetchAllUser = () => {
+    return axios.get("https://reqres.in/api/users?page=2");
+}
+
+export {
+    fetchAllUser
+}
+
