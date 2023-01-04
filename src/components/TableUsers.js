45e75cc6e386e5ee8cdc24a553396e0e9b1f4c5c
Hai Long <hailongkc1203@gmail.com>
2023-01-04 14:01:08 +0700

    Filter User

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -6,9 +6,8 @@ import ReactPaginate from 'react-paginate';
 import ModalAddNew from './ModalAddNew';
 import ModalEditUser from "./ModalEditUser";
 import ModalConfirm from "./ModalConfirm";
-import _ from "lodash";
+import _, { debounce } from "lodash";
 import './TableUsers.scss';
-
 const TableUsers = (props) => {
 
     const [listUsers, setlistUsers] = useState([]);
@@ -25,6 +24,8 @@ const TableUsers = (props) => {
 
     const [sortBy, setSortBy] = useState("asc");
     const [sortField, setSortField] = useState("id");
+
+    // const [keyword, setkeyword] = useState("");
 
     const handleClose = () => {
         setIsShowModalAddNew(false);
@@ -84,115 +85,135 @@ const TableUsers = (props) => {
         setlistUsers(cloneListUsers);
     }
 
+    const handleSearch = debounce((event) => {
+
+        let term = event.target.value;
+
+        console.log(event.target.value);
+        if (term) {
+            let cloneListUsers = _.cloneDeep(listUsers);
+            cloneListUsers = cloneListUsers.filter(item => item.email.includes(term));
+            setlistUsers(cloneListUsers)
+        } else {
+            getUsers(1);
+        }
+    }, 300)
+
     return (<>
         <div className='my-3 add-new'>
             <span> <b>List Users :</b></span>
             <button className='btn btn-success' onClick={() => setIsShowModalAddNew(true)}>Add new user</button>
         </div>
-        <div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th >
-                            <div className="sort-header">
-                                <span>ID</span>
-                                <span>
-                                    <i
-                                        className="fa-solid fa-arrow-down-long"
-                                        onClick={() => handleSort("desc", "id")}
-                                    ></i>
+        <div className="col-4 my-3">
+            <input className="form-control" placeholder="search user by email ..."
 
-                                    <i
-                                        className="fa-solid fa-arrow-up-long"
-                                        onClick={() => handleSort("asc", "id")}
-                                    ></i>
-                                </span>
-                            </div>
-
-
-                        </th>
-                        <th>
-                            <div className="sort-header">
-                                <span>First Name</span>
-                                <span>
-                                    <i
-                                        className="fa-solid fa-arrow-down-long"
-                                        onClick={() => handleSort("desc", "first_name")}
-                                    ></i>
-                                    <i
-                                        className="fa-solid fa-arrow-up-long"
-                                        onClick={() => handleSort("asc", "first_name")}
-                                    ></i>
-                                </span>
-                            </div>
-
-                        </th>
-                        <th >Last Name</th>
-                        <th >Email</th>
-                        <th >Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
-                        return (
-                            <tr key={`users-${index}`}>
-                                <td>{item.id}</td>
-                                <td>{item.first_name}</td>
-                                <td>{item.last_name}</td>
-                                <td>{item.email}</td>
-                                <td>
-                                    <button className="btn btn-warning mx-3"
-                                        onClick={() => handleEdituser(item)}
-                                    >Edit</button>
-                                    <button className="btn btn-danger"
-                                        onClick={() => handleDeleteUser(item)}
-                                    >Delete</button>
-                                </td>
-                            </tr>
-                        )
-
-                    })}
-
-                </tbody>
-            </Table>
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={5}
-                pageCount={totalPages}
-                previousLabel="< previous"
-
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="pagination"
-                activeClassName="active"
-            />
-            <ModalAddNew
-                show={isShowModalAddNew}
-                handleClose={handleClose}
-                handleUpdateTable={handleUpdateTable}
-            />
-            <ModalEditUser
-                show={isShowModalEdit}
-                dataUserEdit={dataUserEdit}
-                handleClose={handleClose}
-                handleUpdateTable={handleUpdateTable}
-                handleEditUserFromModal={handleEditUserFromModal}
-            />
-            <ModalConfirm
-                show={isShowModalDelete}
-                handleClose={handleClose}
-                dataUserDelete={dataUserDelete}
-                handleDeleteUserFromModal={handleDeleteUserFromModal}
+                // value={keyword}
+                onChange={(event) => handleSearch(event)}
             />
         </div>
+
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th >
+                        <div className="sort-header">
+                            <span>ID</span>
+                            <span>
+                                <i
+                                    className="fa-solid fa-arrow-down-long"
+                                    onClick={() => handleSort("desc", "id")}
+                                ></i>
+
+                                <i
+                                    className="fa-solid fa-arrow-up-long"
+                                    onClick={() => handleSort("asc", "id")}
+                                ></i>
+                            </span>
+                        </div>
+
+
+                    </th>
+                    <th>
+                        <div className="sort-header">
+                            <span>First Name</span>
+                            <span>
+                                <i
+                                    className="fa-solid fa-arrow-down-long"
+                                    onClick={() => handleSort("desc", "first_name")}
+                                ></i>
+                                <i
+                                    className="fa-solid fa-arrow-up-long"
+                                    onClick={() => handleSort("asc", "first_name")}
+                                ></i>
+                            </span>
+                        </div>
+
+                    </th>
+                    <th >Last Name</th>
+                    <th >Email</th>
+                    <th >Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
+                    return (
+                        <tr key={`users-${index}`}>
+                            <td>{item.id}</td>
+                            <td>{item.first_name}</td>
+                            <td>{item.last_name}</td>
+                            <td>{item.email}</td>
+                            <td>
+                                <button className="btn btn-warning mx-3"
+                                    onClick={() => handleEdituser(item)}
+                                >Edit</button>
+                                <button className="btn btn-danger"
+                                    onClick={() => handleDeleteUser(item)}
+                                >Delete</button>
+                            </td>
+                        </tr>
+                    )
+
+                })}
+
+            </tbody>
+        </Table>
+        <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={totalPages}
+            previousLabel="< previous"
+
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+        />
+        <ModalAddNew
+            show={isShowModalAddNew}
+            handleClose={handleClose}
+            handleUpdateTable={handleUpdateTable}
+        />
+        <ModalEditUser
+            show={isShowModalEdit}
+            dataUserEdit={dataUserEdit}
+            handleClose={handleClose}
+            handleUpdateTable={handleUpdateTable}
+            handleEditUserFromModal={handleEditUserFromModal}
+        />
+        <ModalConfirm
+            show={isShowModalDelete}
+            handleClose={handleClose}
+            dataUserDelete={dataUserDelete}
+            handleDeleteUserFromModal={handleDeleteUserFromModal}
+        />
     </>
     )
 }
